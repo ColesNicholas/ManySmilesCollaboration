8e785de2324e92059cc71e6fc944a19e4b835d7e
Nicholas A. Coles <40498285+ColesNicholas@users.noreply.github.com>
2021-06-02 15:15:54 -0400

    Add files via upload

--- updates/ManySmiles_Updates.bat
+++ updates/ManySmiles_Updates.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\R\R-4.0.4\bin\Rscript.exe" "C:\Users\ncole\Google Drive\Work\Research\projects\Many Smiles Collaboration\analysis\updates\ManySmiles_Updates.R"
+"C:\Program Files\R\R-4.0.4\bin\Rscript.exe" "C:\Users\ncole\Google Drive\Work\Research\projects\current\Many Smiles Collaboration\analysis\updates\ManySmiles_Updates.R"
 cmd /k
